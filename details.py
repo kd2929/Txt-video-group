f608a71d75e31c141f783aa09bbf84b2b7f29da5
Amanraj654 <paymentaman6+github@gmail.com>
2023-04-13 04:34:55 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 9980982
-api_hash = "a6198445d05b755ec866c03b3358197b"
-bot_token = "5044521147:AAENtWyeVYrWsDwWu42QN96RpCyCE45f1Nc"
+api_id = 23365392
+api_hash = "fffdba007ae9e02207be11b8d8d3f2a8"
+bot_token = "6267374965:AAGSjyPkpU3tid4njiOBvgIy7SMAbzhxTdw"
 
